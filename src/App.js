1b51f88a27e1d1887d9cf81804b03cd28d7c2b49
ganesh.bmh <ganesh.bmh1@gmail.com>
2024-03-16 23:30:46 +0530

    main files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import CustomerRegistration from "./Components/CustomerRegistration/CustomerRegistration";
+import LandingPage from "./Components/LandingPage/LandingPage";
+import Login from "./Components/Login/Login";
+import AckSucess from "./Components/AckSucess/AckSucess";
+import HomePage from "./Components/HomePage/HomePage";
+import PolicySelection from "./Components/PolicySelection/PolicySelection";
+import PolicyView from "./Components/PolicyView/PolicyView";
+import Nominee from "./Components/Nominee/Nominee";
+import Blog from "./Components/Blog/Blog";
+import Contact from "./Components/Contact/Contact";
+import Feedback from "./Components/Feedback/Feedback";
 
 function App() {
+  const [component, setComponent] = useState("LandingPage");
+
+  function loadComponent() {
+    console.log(component);
+
+    switch (component) {
+      case "LandingPage":
+        return <LandingPage setComponent={setComponent} />;
+      case "CustomerRegistration":
+        return <CustomerRegistration setComponent={setComponent} />;
+      case "Login":
+        return <Login setComponent={setComponent} />;
+      case "HomePage":
+        return <HomePage setComponent={setComponent} />;
+      case "PolicySelection":
+        return <PolicySelection setComponent={setComponent} />;
+      case "PolicyView":
+        return <PolicyView setComponent={setComponent} />;
+      case "AckSucess":
+        return <AckSucess setComponent={setComponent} />;
+      case "Nominee":
+        return <Nominee setComponent={setComponent} />;
+      case "Blog":
+        return <Blog setComponent={setComponent} />;
+      case "Contact":
+        return <Contact setComponent={setComponent} />;
+      case "Feedback":
+        return <Feedback setComponent={setComponent} />;
+      default:
+        break;
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Happy <br />
+          <span>Insurance Online</span>
+        </h1>
       </header>
+      <div className="content">{loadComponent()}</div>
+      <footer>
+        <p>@Happy Insurance Online ❤</p>
+      </footer>
     </div>
   );
 }
